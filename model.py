f248c54c9591b7db1918b90138b0225cf6366a42
tejablazic <tejablazic12@gmail.com>
2025-02-26 14:49:25 +0100

    spletni vmesnik in pogledi

--- model.py
+++ model.py
@@ -1,34 +1,30 @@
 import sqlite3
 
-conn = sqlite3.connect('baza.sqlite')
+conn = sqlite3.connect("baza.sqlite")
 
 class Stranka:
 
     def __init__(self, ime, id_stranke):
-        self.ime = ime
         self.id_stranke = id_stranke
+        self.ime = ime
 
     def __repr__(self):
-        return f'Stranka({self.ime})'
+        return f"Stranka({self.ime})"
     
     def __str__(self):
         return self.ime
     
     @staticmethod
     def vse_stranke():
-        poizvedba = '''SELECT id, ime FROM stranka'''
+        poizvedba = """SELECT id, ime FROM stranka"""
         for id_stranke, ime in conn.execute(poizvedba):
             yield Stranka(ime, id_stranke)
 
     @staticmethod
     def najdi_stranko(id_stranke):
-        poizvedba = '''SELECT id, ime FROM stranka WHERE id = ?'''
+        poizvedba = """SELECT id, ime FROM stranka WHERE id = ?"""
         id_stranke, ime = conn.execute(poizvedba, [id_stranke]).fetchone()
         return Stranka(ime, id_stranke)
-
-# for rez in Stranka.vse_stranke():
-#     print(rez)
-
 
 class Narocilo:
 
@@ -36,13 +32,13 @@ class Narocilo:
         self.id_narocila = id_narocila
         self.id_stranke = id_stranke
         self.kolicina = kolicina
-
+    
     @staticmethod
     def narocila_stranke(stranka):
-        poizvedba = '''
-        SELECT id, kolicina FROM narocilo
+        poizvedba = """
+        SELECT id, kolicina
+        FROM narocilo
         WHERE stranka = ?
-        '''
+        """
         for id_narocila, kolicina in conn.execute(poizvedba, [stranka.id_stranke]):
             yield Narocilo(id_narocila, stranka.id_stranke, kolicina)
-
