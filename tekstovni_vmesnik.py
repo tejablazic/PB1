9f3bdb502c401c2d5abde25e40e77d6355b6b7de
tejablazic <tejablazic12@gmail.com>
2025-02-26 14:52:43 +0100

    preprost tekstovni vmesnik

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -2,33 +2,34 @@ from model import Stranka, Narocilo
 
 def narocila_stranke():
     stranke = list(Stranka.vse_stranke())
-    print('Izberi stranko')
+    print("Izberi stranko")
     for i, stranka in enumerate(stranke, 1):
         print(f"{i} {stranka}")
-    izbira = int(input("> ")) - 1 # - 1 ker smo začeli štet z 1
-    izbrano_ime = stranke[izbira]
-    narocila = Narocilo.narocila_stranke(izbrano_ime)
+    izbira = int(input("> ")) - 1
+    izbrana_stranka = stranke[izbira]
+    narocila = Narocilo.narocila_stranke(izbrana_stranka)
     for i, narocilo in enumerate(narocila, 1):
-        print(f'{i} {narocilo.kolicina}')
+        print(f"{narocilo.id_narocila} {narocilo.kolicina}")
 
 def narocila_po_kolicini():
     raise NotImplementedError
 
+
 def pozeni():
     while True:
         print(
-            'Izberi možnost:',
-            '1 Naročila stranke',
-            '2 Naročila po količini:',
-            '3 Zaključi', sep = '\n'
+            "Izberi možnost:", 
+            "1 Naročila stranke", 
+            "2 Naročila po količini", 
+            "3 Zaključi", sep="\n"
         )
-        izbira = input('> ')
-        if izbira == '1':
+        izbira = input("> ")
+        if izbira == "1":
             narocila_stranke()
-        elif izbira == '2':
+        elif izbira == "2":
             narocila_po_kolicini()
-        elif izbira == '3':
-            print('Nasvidenje!')
+        elif izbira == "3":
+            print("Na svidenje!")
             break
 
 pozeni()
